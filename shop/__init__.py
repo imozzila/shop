2918f15616191ce296a2b889fac7daac2befc710
Lewis Mullarkey <mullarkeyl@cardiff.ac.uk>
2020-03-11 14:12:10 +0000

    reorganising files

--- shop/__init__.py
+++ shop/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '<paste SECRET_KEY here>'
+app.config['SECRET_KEY'] = 'f256182c79e170d3561f6dc8ffdd9ebd4c1a32c99df6b4e3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://C1930597:Kys123kys123@csmysql.cs.cf.ac.uk:3306/C1930597_shop'
 db = SQLAlchemy(app)
 

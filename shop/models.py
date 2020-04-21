3cf2e0194048aea3181bbb30117884f672a31a15
Lewis Mullarkey <mullarkeyl@cf.ac.uk>
2020-04-21 02:59:17 +0100

    added login & registration system

--- shop/models.py
+++ shop/models.py
@@ -12,14 +12,14 @@ class User(db.Model):
     UserId = db.Column(db.Integer, primary_key=True, nullable=False)
     UserName = db.Column(db.String(255), nullable=False)
     UserPassword = db.Column(db.String(255), nullable=False)
-    UserEmail = db.Column(db.string(255), nullable=False)
+    UserEmail = db.Column(db.String(255), nullable=False)
 
 class Basket(db.Model):
     BasketId = db.Column(db.Integer, primary_key=True, nullable=False)
-    UserId = db.Column(db.Integer, ForeignKey("User.UserId"), nullable=False)
-    ItemId = db.Column(db.Integer, ForeignKey("Item.ItemId"), nullable=False)
+    UserId = db.Column(db.Integer, db.ForeignKey("User.UserId"), nullable=False)
+    ItemId = db.Column(db.Integer, db.ForeignKey("Item.ItemId"), nullable=False)
 
 class WishList(db.Model):
     WishListId= db.Column(db.Integer, primary_key=True, nullable=False)
-    UserId = db.Column(db.Integer, ForeignKey("User.UserId"), nullable=False)
-    ItemId = db.Column(db.Integer, ForeignKey("Item.ItemId"), nullable=False)
+    UserId = db.Column(db.Integer, db.ForeignKey("User.UserId"), nullable=False)
+    ItemId = db.Column(db.Integer, db.ForeignKey("Item.ItemId"), nullable=False)
--- shop/routes.py
+++ shop/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from shop import app
+from shop import forms
+from shop.models import Item, User, Basket, WishList, db
 
 @app.route('/')
 def home():
@@ -8,3 +10,32 @@ def home():
 @app.route('/basket/<int:BasketId>')
 def shopping_basket(BasketId):
     basket=Basket.query().get_or_404(BasketId)
+
+@app.route('/checkout/<int:BasketId>')
+def checkout(BasketId):
+    basket=Basket.query().get_or_404(BasketId)
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        new_user = User(UserName=form.username.data, UserEmail=form.email.data, UserPassword=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return '<h1>New user has been created!</h1>'
+    return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(UserName=form.username.data).first()
+        if user:
+            if user.UserPassword == form.password.data:
+                return redirect(url_for('dashboard'))
+        return '<h1>Invalid username or password.</h1>'
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')

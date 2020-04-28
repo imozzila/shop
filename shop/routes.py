b7829db28fe092acc839724187690f5764df9638
Lewis Mullarkey <mullarkeyl@cf.ac.uk>
2020-04-28 15:28:48 +0100

    fixed button

--- shop/routes.py
+++ shop/routes.py
@@ -24,10 +24,11 @@ def home():
 
 @app.route('/basket/')
 def shopping_basket():
+    pages = organise_pages()
     if not current_user.is_anonymous:
         contents = []
         user = load_user(current_user.UserId)
-        pages = organise_pages()
+
         basket = Basket.query.filter_by(UserId=user.UserId)
         totalprice = 0
         for entry in basket:
@@ -35,7 +36,7 @@ def shopping_basket():
             totalprice += item.ItemPrice
             contents.append(item)
         return render_template('basket.html', pages=pages, page_list=list(pages.keys()), contents=contents, totalprice=totalprice)
-    return 'You are not logged in. You must be logged in to view basket.'
+    return redirect('error')
 
 @app.route('/checkout', methods=['GET', 'POST'])
 def checkout():
@@ -102,6 +103,12 @@ def settings():
 def logout():
     logout_user()
     return redirect('/home')
+
+@app.route('/error')
+def error():
+    pages=organise_pages()
+    
+    return render_template('error.html', pages=pages, page_list=list(pages.keys()))
 
 @login_manager.user_loader
 def load_user(user_id):
